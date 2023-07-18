14af91dd9c1935415dd24d44c5710609ecc2a63f
Yujin Kim <cocodj0509@naver.com>
2023-07-18 23:58:44 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import uploadImg from "./asset/upload.png";
+import { useRef, useState, useCallback } from "react";
+import { ImageConfig } from "./config/ImageConfig";
 
 function App() {
+  const wrapRef = useRef(null);
+  const [fileList, setFileList] = useState([]);
+
+  const onDragOver = (e) => {
+    e.preventDefault();
+  };
+  const onDragEnter = (e) => {
+    e.preventDefault();
+    wrapRef.current.classList.add("on-drag");
+  };
+  const onDragLeave = (e) => {
+    e.preventDefault();
+    wrapRef.current.classList.remove("on-drag");
+  };
+  const onDrop = (e) => {
+    e.preventDefault();
+    const newFile = e.dataTransfer.files[0];
+    if (e.dataTransfer.files[0]) {
+      setFileList([...fileList, newFile]);
+    }
+  };
+
+  const onFileDrop = (e) => {
+    e.preventDefault();
+    const newFile = e.target.files[0];
+    if (newFile) {
+      setFileList([...fileList, newFile]);
+    }
+  };
+
+  const removeFile = (item) => {
+    setFileList(fileList.filter((remove) => remove !== item));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="drag-container">
+        <h2>drag and drop</h2>
+        {console.log(fileList)}
+        <input
+          style={{ display: "none" }}
+          type="file"
+          name="file"
+          id="file"
+          onChange={onFileDrop}
+        />
+        <label htmlFor="file">
+          <div
+            className="label-container"
+            ref={wrapRef}
+            onDragOver={onDragOver}
+            onDragEnter={onDragEnter}
+            onDragLeave={onDragLeave}
+            onDrop={onDrop}
+          >
+            <img src={uploadImg} alt="upload" />
+          </div>
+        </label>
+        {fileList.length > 0 ? (
+          <div>
+            <p>Ready to upload</p>
+            {fileList.map((item, index) => {
+              return (
+                <div key={index} className="fileList-container">
+                  <img
+                    src={
+                      ImageConfig[item.type.split("/")[1]]
+                        ? ImageConfig[item.type.split("/")[1]]
+                        : ImageConfig["default"]
+                    }
+                  />
+                  <span className="fileList-string">
+                    <p>{item.name}</p>
+                    <p>{item.size}</p>
+                  </span>
+
+                  <button
+                    className="remove-file"
+                    onClick={() => removeFile(item)}
+                  >
+                    X
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
---
+++ src/config/ImageConfig.js
@@ -0,0 +1,11 @@
+import fileDefault from "../asset/file-blank-solid-240.png";
+import fileCSS from "../asset/file-css-solid-240.png";
+import filePdf from "../asset/file-pdf-solid-240.png";
+import filePng from "../asset/file-png-solid-240.png";
+
+export const ImageConfig = {
+  default: fileDefault,
+  pdf: filePdf,
+  png: filePng,
+  css: fileCSS,
+};
